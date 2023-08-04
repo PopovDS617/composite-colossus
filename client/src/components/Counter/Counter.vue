e8325fc42bae8ba1b902f4c225b84f7798365c9d
PopovDS617 <popovds.bm@gmail.com>
2023-08-04 21:10:38 +0300

    feat: add props

--- client/src/components/Counter/Counter.vue
+++ client/src/components/Counter/Counter.vue
@@ -5,11 +5,14 @@ import Btn from './Button/Btn.vue';
 import { ref } from 'vue';
 
 const count = ref(0);
+
+const plus = () => count.value++;
+const minus = () => count.value--;
 </script>
 
 <template>
   <div class="counter-container">
     <Display count="count" />
-    <div class="controls"><Btn /> <Btn /></div>
+    <div class="controls"><Btn onClick="minus" /> <Btn onClick="plus" /></div>
   </div>
 </template>
