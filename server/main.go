71502cb6da892fb33e880f9800cba8a886d16733
Popov Dmitry <popovds.bm@gmail.com>
2024-01-12 14:36:27 +0300

    feat: setup; start up server; add types

--- server/main.go
+++ server/main.go
@@ -1,3 +1,24 @@
 package main
 
-func main() {}
+import (
+	"app/api"
+	"flag"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+
+	listenAddr := flag.String("listenAddr", ":5000", "The listen address of the API server")
+	flag.Parse()
+
+	app := fiber.New()
+
+	apiV1 := app.Group("api/v1")
+
+	apiV1.Get("/user", api.HandleGetUsers)
+	apiV1.Get("/user/:id", api.HandleGetUserById)
+
+	app.Listen(*listenAddr)
+
+}
