f2668ab61ee5880b499b148e3ebdc96462ce5b4e
Popov Dmitry <popovds.bm@gmail.com>
2024-01-12 16:52:59 +0300

    feat: add mongodb

--- server/main.go
+++ server/main.go
@@ -2,12 +2,45 @@ package main
 
 import (
 	"app/api"
+	"app/types"
+	"context"
 	"flag"
+	"fmt"
+
+	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+const dburi = "mongodb://root:password@localhost:27017/my_db?authSource=admin"
+const dbname = "my_db"
+const userColl = "users"
+
 func main() {
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx := context.Background()
+
+	user := types.User{
+		FirstName: "Mark",
+		LastName:  "One",
+	}
+
+	coll := client.Database(dbname).Collection(userColl)
+	res, err := coll.InsertOne(ctx, user)
+
+	if err != nil {
+		log.Fatal()
+	}
+
+	fmt.Println(res)
 
 	listenAddr := flag.String("listenAddr", ":5000", "The listen address of the API server")
 	flag.Parse()
