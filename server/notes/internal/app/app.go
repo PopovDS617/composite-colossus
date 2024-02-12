13a5fee974362cc84a2b6f91a2b49543dfbbfca9
Popov Dmitry <popovds.bm@gmail.com>
2024-02-12 17:33:38 +0300

    chore: fix envs; add db tags

--- server/notes/internal/app/app.go
+++ server/notes/internal/app/app.go
@@ -57,7 +57,7 @@ func (a *App) initDeps(ctx context.Context) error {
 }
 
 func (a *App) initConfig(_ context.Context) error {
-	err := config.Load(".env")
+	err := config.Load("../../local.env")
 	if err != nil {
 		return err
 	}
