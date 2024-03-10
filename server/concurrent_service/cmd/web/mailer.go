787653d93431d2fa2d1169c80f601b0b8ec78972
Popov Dmitry <popovds.bm@gmail.com>
2024-03-10 21:32:04 +0300

    feat: add mailer

---
+++ server/concurrent_service/cmd/web/mailer.go
@@ -0,0 +1,193 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"sync"
+	"time"
+
+	"github.com/vanng822/go-premailer/premailer"
+	mail "github.com/xhit/go-simple-mail/v2"
+)
+
+type Mail struct {
+	Domain      string
+	Host        string
+	Port        int
+	Username    string
+	Password    string
+	Encryption  string
+	FromAddress string
+	FromName    string
+	Wait        *sync.WaitGroup
+	MailerChan  chan Message
+	ErrorChan   chan error
+	Done        chan bool
+}
+
+type Message struct {
+	From        string
+	FromName    string
+	To          string
+	Subject     string
+	Attachments []string
+	Data        any
+	DataMap     map[string]any
+	Template    string
+}
+
+func (m *Mail) sendMail(msg Message, errorChan chan error) {
+	if msg.Template == "" {
+		msg.Template = "mail"
+	}
+
+	if msg.From == "" {
+		msg.From = m.FromAddress
+
+	}
+
+	if msg.FromName == "" {
+		msg.FromName = m.FromName
+	}
+
+	data := map[string]any{
+		"message": msg.Data,
+	}
+
+	msg.DataMap = data
+
+	formattedMessage, err := m.BuildHTMLMessage(msg)
+
+	if err != nil {
+		errorChan <- err
+	}
+	fmt.Println("messages created")
+	plainMessage, err := m.BuildPlainTextMessage(msg)
+	if err != nil {
+		errorChan <- err
+	}
+
+	server := mail.NewSMTPClient()
+	server.Host = m.Host
+	server.Port = m.Port
+	server.Username = m.Username
+	server.Password = m.Password
+	server.Encryption = m.getEncryption(m.Encryption)
+	server.KeepAlive = false
+	server.ConnectTimeout = 10 * time.Second
+	server.SendTimeout = 10 * time.Second
+
+	smtpClient, err := server.Connect()
+
+	fmt.Println("client", smtpClient)
+
+	if err != nil {
+		errorChan <- err
+	}
+
+	email := mail.NewMSG()
+	email.SetFrom(msg.From).AddTo(msg.To).SetSubject(msg.Subject)
+
+	email.SetBody(mail.TextPlain, plainMessage)
+	email.AddAlternative(mail.TextHTML, formattedMessage)
+
+	if len(msg.Attachments) > 0 {
+		for _, v := range msg.Attachments {
+			email.AddAttachment(v)
+		}
+	}
+	err = email.Send(smtpClient)
+
+	fmt.Println("here")
+
+	if err != nil {
+		errorChan <- err
+	}
+
+}
+
+func (m *Mail) BuildHTMLMessage(msg Message) (string, error) {
+	templateToRender := fmt.Sprintf("./cmd/web/templates/%s/html.gohtml", msg.Template)
+
+	t, err := template.New("email-html").Parse(templateToRender)
+
+	if err != nil {
+		return "", err
+	}
+
+	var tpl bytes.Buffer
+
+	if err = t.ExecuteTemplate(&tpl, "body", msg.DataMap); err != nil {
+		return "", err
+	}
+	fmt.Println("messages created")
+
+	formattedMessage := tpl.String()
+	formattedMessage, err = m.inlineCSS(formattedMessage)
+
+	if err != nil {
+		return "", err
+	}
+
+	fmt.Println("formatted message is", formattedMessage)
+
+	return formattedMessage, nil
+}
+
+func (m *Mail) BuildPlainTextMessage(msg Message) (string, error) {
+
+	templateToRender := fmt.Sprintf("./cmd/web/templates/%s/plain.gohtml", msg.Template)
+
+	t, err := template.New("email-html").Parse(templateToRender)
+
+	if err != nil {
+		return "", err
+	}
+
+	var tpl bytes.Buffer
+
+	if err = t.ExecuteTemplate(&tpl, "body", msg.DataMap); err != nil {
+		return "", err
+
+	}
+
+	plainMessage := tpl.String()
+
+	return plainMessage, nil
+}
+
+func (m *Mail) getEncryption(e string) mail.Encryption {
+	switch e {
+	case "tls":
+		return mail.EncryptionSTARTTLS
+	case "ssl":
+		return mail.EncryptionSSLTLS
+	case "none":
+		return mail.EncryptionNone
+	default:
+		return mail.EncryptionSTARTTLS
+	}
+}
+
+func (m *Mail) inlineCSS(s string) (string, error) {
+	options := premailer.Options{
+		RemoveClasses:     false,
+		CssToAttributes:   false,
+		KeepBangImportant: true,
+	}
+
+	prem, err := premailer.NewPremailerFromString(s, &options)
+	if err != nil {
+		return "", err
+	}
+
+	html, err := prem.Transform()
+
+	if err != nil {
+		return "", err
+	}
+
+	return html, nil
+
+}
