06a4d87f0ac65fc59c1b4e739ee2e25b6094c619
Popov Dmitry <popovds.bm@gmail.com>
2024-03-16 17:20:43 +0300

    feat: change models, repository

--- server/withPSQL/internal/app/app.go
+++ server/withPSQL/internal/app/app.go
@@ -5,10 +5,14 @@ import (
 	"net/http"
 
 	"withpsql/internal/closer"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 type App struct {
 	serviceProvider *serviceProvider
+	router          *chi.Mux
 }
 
 func NewApp(ctx context.Context) (*App, error) {
@@ -27,14 +31,13 @@ func (a *App) Run() error {
 		closer.CloseAll()
 		closer.Wait()
 	}()
-
-	return http.ListenAndServe("localhost:9000", a.serviceProvider.animalImpl.Router(context.Background()))
+	return a.runHTTPServer()
 }
 
 func (a *App) initDeps(ctx context.Context) error {
 	inits := []func(context.Context) error{
-
 		a.initServiceProvider,
+		a.initHTTPServer,
 	}
 
 	for _, f := range inits {
@@ -50,4 +53,24 @@ func (a *App) initDeps(ctx context.Context) error {
 func (a *App) initServiceProvider(_ context.Context) error {
 	a.serviceProvider = newServiceProvider()
 	return nil
+}
+
+func (a *App) initHTTPServer(ctx context.Context) error {
+
+	r := chi.NewRouter()
+
+	animal := a.serviceProvider.AnimalImpl(ctx)
+	animalRouter := animal.Router(ctx)
+
+	r.Use(middleware.Recoverer)
+
+	r.Mount("/", animalRouter)
+
+	a.router = r
+
+	return nil
+}
+
+func (a *App) runHTTPServer() error {
+	return http.ListenAndServe("localhost:9000", a.router)
 }
--- server/withPSQL/internal/repository/animal/repository.go
+++ server/withPSQL/internal/repository/animal/repository.go
@@ -2,23 +2,35 @@ package animal
 
 import (
 	"context"
+	"fmt"
+	"log"
 
 	sq "github.com/Masterminds/squirrel"
 
 	"withpsql/internal/client/db"
 	"withpsql/internal/model"
+	"withpsql/internal/repository/animal/converter"
+	repoModel "withpsql/internal/repository/animal/model"
 
 	"withpsql/internal/repository"
 )
 
 const (
-	tableName = "animal"
+	animalsTableName = "animals"
+	regionsTableName = "regions"
 
-	idColumn        = "id"
-	titleColumn     = "title"
-	contentColumn   = "content"
-	createdAtColumn = "created_at"
-	updatedAtColumn = "updated_at"
+	idColumn              = "animals.id"
+	nameColumn            = "animals.name"
+	typeColumn            = "animals.type"
+	genderColumn          = "animals.gender"
+	ageColumn             = "animals.age"
+	createdAtColumn       = "animals.created_at"
+	updatedAtColumn       = "animals.updated_at"
+	lastTimeSeenAtColumn  = "animals.last_time_seen_at"
+	animalsRegionIDColumn = "animals.region_id"
+	regionsRegionIDColumn = "regions.region_id"
+	seenByDeviceIDColumn  = "animals.seen_by_device_id"
+	regionNameColumn      = "regions.region_name"
 )
 
 type repo struct {
@@ -29,10 +41,10 @@ func NewRepository(db db.Client) repository.AnimalRepository {
 	return &repo{db: db}
 }
 
-func (r *repo) Create(ctx context.Context, info *model.Animal) (int64, error) {
-	builder := sq.Insert(tableName).
+func (r *repo) Create(ctx context.Context, animal *model.Animal) (int64, error) {
+	builder := sq.Insert(animalsTableName).
 		PlaceholderFormat(sq.Dollar).
-		Columns(titleColumn, contentColumn).
+		Columns(idColumn, nameColumn).
 		// Values(info.Title, info.Content).
 		Suffix("RETURNING id")
 
@@ -56,10 +68,11 @@ func (r *repo) Create(ctx context.Context, info *model.Animal) (int64, error) {
 }
 
 func (r *repo) Get(ctx context.Context, id int64) (*model.Animal, error) {
-	builder := sq.Select(idColumn, titleColumn, contentColumn, createdAtColumn, updatedAtColumn).
+	builder := sq.Select(idColumn, nameColumn, typeColumn, ageColumn, genderColumn, createdAtColumn, updatedAtColumn, animalsRegionIDColumn, lastTimeSeenAtColumn, seenByDeviceIDColumn, regionNameColumn).
 		PlaceholderFormat(sq.Dollar).
-		From(tableName).
+		From(animalsTableName).
 		Where(sq.Eq{idColumn: id}).
+		InnerJoin(fmt.Sprintf("%s ON %s = %s", regionsTableName, animalsRegionIDColumn, regionsRegionIDColumn)).
 		Limit(1)
 
 	query, args, err := builder.ToSql()
@@ -72,11 +85,78 @@ func (r *repo) Get(ctx context.Context, id int64) (*model.Animal, error) {
 		QueryRaw: query,
 	}
 
-	var animal model.Animal
-	err = r.db.DB().QueryRowContext(ctx, q, args...).Scan(&animal.ID, &animal.Name, &animal.Type, &animal.Age, &animal.CreatedAt, &animal.UpdatedAt)
+	var animal repoModel.Animal
+	err = r.db.DB().QueryRowContext(ctx, q, args...).Scan(
+		&animal.ID,
+		&animal.Name,
+		&animal.Type,
+		&animal.Age,
+		&animal.Gender,
+		&animal.CreatedAt,
+		&animal.UpdatedAt,
+		&animal.RegionID,
+		&animal.LastTimeSeenAt,
+		&animal.SeenByDevice,
+		&animal.RegionName,
+	)
+
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	return converter.FromAnimalRepoToAnimalModel(&animal), nil
+}
+
+func (r *repo) GetAll(ctx context.Context) ([]*model.Animal, error) {
+	builder := sq.Select(idColumn, nameColumn, typeColumn, ageColumn, genderColumn, createdAtColumn, updatedAtColumn, animalsRegionIDColumn, lastTimeSeenAtColumn, seenByDeviceIDColumn, regionNameColumn).
+		PlaceholderFormat(sq.Dollar).
+		From(animalsTableName).
+		InnerJoin(fmt.Sprintf("%s ON %s = %s", regionsTableName, animalsRegionIDColumn, regionsRegionIDColumn))
+
+	query, args, err := builder.ToSql()
 	if err != nil {
 		return nil, err
 	}
 
-	return &animal, nil
+	q := db.Query{
+		Name:     "animal_repository.GetAll",
+		QueryRaw: query,
+	}
+
+	err = r.db.DB().QueryRowContext(ctx, q, args...).Scan()
+
+	rows, err := r.db.DB().QueryContext(ctx, q, args...)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	var animals []*model.Animal
+	for rows.Next() {
+		var repoAnimal repoModel.Animal
+
+		err := rows.Scan(&repoAnimal.ID,
+			&repoAnimal.Name,
+			&repoAnimal.Type,
+			&repoAnimal.Age,
+			&repoAnimal.Gender,
+			&repoAnimal.CreatedAt,
+			&repoAnimal.UpdatedAt,
+			&repoAnimal.RegionID,
+			&repoAnimal.LastTimeSeenAt,
+			&repoAnimal.SeenByDevice,
+			&repoAnimal.RegionName)
+
+		if err != nil {
+			log.Println("Error scanning", err)
+			return nil, err
+		}
+		converted := converter.FromAnimalRepoToAnimalModel(&repoAnimal)
+
+		animals = append(animals, converted)
+
+	}
+
+	return animals, nil
 }
